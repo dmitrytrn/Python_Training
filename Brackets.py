8dfbd02e57b700679a10f25d37425b07b372c39b
DmitrTRC <9984398@gmail.com>
2021-03-09 17:32:11 +0300

    Change_letter: Done

---
+++ Brackets.py
@@ -0,0 +1,2 @@
+source_str = input()
+print(source_str[source_str.index('(') + 1: source_str.index(')')])
---
+++ Change_letter.py
@@ -0,0 +1,5 @@
+source_str = input()
+print(f'{source_str.replace("a", "o")} ,'
+      f' {source_str.count("a")} letters replaced,'
+      f' String Length : {len(source_str)}'
+      )
